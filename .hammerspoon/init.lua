fe16c06c4491c0f71ddfc3fdf386a9aa1f87d715
Juyeong Park <sprnd645@gmail.com>
2023-12-19 18:40:55 +0900

    Update init.lua

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -29,31 +29,42 @@ end
 
 --- quick open applications
 --- https://zhiye.li/hammerspoon-use-the-keyboard-shortcuts-to-launch-apps-a7c59ab3d92
+hs.hotkey.bind({"alt", "shift"}, "L", appLaunchOrFocus("RunJS"))
+
 hs.hotkey.bind({"alt", "shift"}, ";", appLaunchOrFocus("Finder"))
-hs.hotkey.bind({"alt", "shift"}, "space", appLaunchOrFocus("Iterm"))
+hs.hotkey.bind({"alt", "shift"}, "space", appLaunchOrFocus("Iterm 2"))
 
-hs.hotkey.bind({"alt", "shift"}, "I", appLaunchOrFocus("Intellij IDEA"))
 hs.hotkey.bind({"alt", "shift"}, "V", appLaunchOrFocus("Visual Studio Code"))
+hs.hotkey.bind({"alt", "shift"}, "I", appLaunchOrFocus("Intellij IDEA"))
+hs.hotkey.bind({"alt", "shift"}, "W", appLaunchOrFocus("WebStorm"))
 hs.hotkey.bind({"alt", "shift"}, "F", appLaunchOrFocus("Figma"))
+--- hs.hotkey.bind({"alt", "shift"}, "X", appLaunchOrFocus("NoSQLBooster for MongoDB"))
+hs.hotkey.bind({"alt", "shift"}, "X", appLaunchOrFocus("DataGrip"))
+--- hs.hotkey.bind({"alt", "shift"}, "A", appLaunchOrFocus("Sequel Pro"))
 
-hs.hotkey.bind({"alt", "shift"}, "A", appLaunchOrFocus("AnyDesk"))
+--- hs.hotkey.bind({"alt", "shift"}, "A", appLaunchOrFocus("App Store"))
+hs.hotkey.bind({"alt", "shift"}, "A", appLaunchOrFocus("Excalidraw"))
 
 hs.hotkey.bind({"alt", "shift"}, "G", appLaunchOrFocus("Google Chrome"))
 
+--- 크롬이랑 가까운키 남는거로
+hs.hotkey.bind({"alt", "shift"}, "T", appLaunchOrFocus("Postman"))
+
 hs.hotkey.bind({"alt", "shift"}, "Y", appLaunchOrFocus("Youtube"))
 hs.hotkey.bind({"alt", "shift"}, ".", appLaunchOrFocus("Youtube Music"))
+--- hs.hotkey.bind({"alt", "shift"}, "/", appLaunchOrFocus("Melon"))
 
 hs.hotkey.bind({"alt", "shift"}, "E", appLaunchOrFocus("Microsoft Excel"))
 hs.hotkey.bind({"alt", "shift"}, "N", appLaunchOrFocus("Notion"))
 --- hs.hotkey.bind({"alt", "shift"}, "H", appLaunchOrFocus("HwpMac2014VIEWER.app"))
-hs.hotkey.bind({"alt", "shift"}, "W", appLaunchOrFocus("Microsoft Word"))
+hs.hotkey.bind({"alt", "shift"}, "W", appLaunchOrFocus("WebStorm"))
 hs.hotkey.bind({"alt", "shift"}, "R", appLaunchOrFocus("Preview"))
 hs.hotkey.bind({"alt", "shift"}, "P", appLaunchOrFocus("Microsoft PowerPoint"))
 
 hs.hotkey.bind({"alt", "shift"}, "K", appLaunchOrFocus("KakaoTalk"))
 hs.hotkey.bind({"alt", "shift"}, "D", appLaunchOrFocus("Discord"))
 hs.hotkey.bind({"alt", "shift"}, "O", appLaunchOrFocus("Docker"))
-hs.hotkey.bind({"alt", "shift"}, "T", appLaunchOrFocus("Telegram"))
+--- hs.hotkey.bind({"alt", "shift"}, "T", appLaunchOrFocus("Telegram"))
 hs.hotkey.bind({"alt", "shift"}, "S", appLaunchOrFocus("Slack"))
 hs.hotkey.bind({"alt", "shift"}, "M", appLaunchOrFocus("Messages"))
 
@@ -64,11 +75,35 @@ hs.hotkey.bind({"alt", "shift"}, ",", appLaunchOrFocus("System Preferences"))
 hs.hotkey.bind({"alt", "shift"}, "h", appLaunchOrFocus("Hammerspoon"))
 hs.hotkey.bind({"alt", "shift"}, "B", appLaunchOrFocus("Be Focused"))
 
+hs.hotkey.bind({"alt", "shift"}, "J", appLaunchOrFocus("ChatGPT"))
+
 function setVolumeZero()
-    hs.audiodevice.defaultOutputDevice():setVolume(0)
+    local currentNetwork = hs.wifi.currentNetwork()
+    print(currentNetwork)
+
+    if (currentNetwork == nil) then
+        print('No network')
+        return
+    end
+
+    -- 집 wifi면
+    if currentNetwork == "KT_GiGA_5G_AF33" or currentNetwork == "KT_GiGA_Mesh_AF33" then
+        print('집 wifi --------------------------------')
+        return
+    end
+
+    local currentDevice = hs.audiodevice.defaultOutputDevice()
+    if currentDevice:name() == "MacBook Pro 스피커" then
+        print('볼륨 0으로 설정')
+        print(currentDevice:name())
+        print(currentNetwork)
+        hs.audiodevice.defaultOutputDevice():setVolume(0)
+    end
 end
 
 wifiWatcher = hs.wifi.watcher.new(setVolumeZero)
 wifiWatcher:start()
 
-hs.audiodevice.defaultOutputDevice():setVolume(0)
+-- 무조건 볼륨 0으로 설정
+-- hs.audiodevice.defaultOutputDevice():setVolume(0)
+
