ecda547aa9e786f7f0fe98f78ffca6af10b281ef
jxmen <sprnd645@gmail.com>
2024-05-10 12:14:46 +0900

    leetcode.nvim 주석 추가

--- .config/nvim/lua/plugins/leetcode.lua
+++ .config/nvim/lua/plugins/leetcode.lua
@@ -13,7 +13,7 @@ return {
   },
   opts = {
     -- configuration goes here
-    arg = "leetcode.nvim",
-    lang = "java",
+    arg = "leetcode.nvim", -- nvim 뒤에 어떤 명령어를 입력해야 leetcode 창을 띄울지 설정
+    lang = "java", -- 문제를 열 시 기본 언어
   },
 }
